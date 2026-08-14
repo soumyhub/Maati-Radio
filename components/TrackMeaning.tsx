7afd5621f0a81a9bb6983aba97d8b9ebdd671068
soumyhub <workspaliwal@gmail.com>
2026-08-15 04:53:19 +0530

    Update TrackMeaning.tsx

--- components/TrackMeaning.tsx
+++ components/TrackMeaning.tsx
@@ -8,7 +8,7 @@ interface TrackMeaningProps {
 
 export default function TrackMeaning({ track }: TrackMeaningProps) {
   return (
-    <div className="pointer-events-none fixed left-1/2 top-[calc(100%-20.5%)] z-[11] w-[min(88vw,680px)] -translate-x-1/2 text-center">
+    <div className="pointer-events-none absolute left-1/2 top-full z-[11] mt-3 w-[min(88vw,680px)] -translate-x-1/2 text-center">
       {/* Label */}
       <p className="mb-2 font-[family-name:var(--font-ui)] text-[0.55rem] uppercase tracking-[0.3em] text-cream/40">
         From the Track
