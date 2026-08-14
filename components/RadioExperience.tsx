9ae33050a27ee32489e156f8a27dd7a5acfb35fd
soumyhub <workspaliwal@gmail.com>
2026-08-14 15:01:17 +0530

    Update RadioExperience.tsx

--- components/RadioExperience.tsx
+++ components/RadioExperience.tsx
@@ -5,15 +5,23 @@ import TopBar from "./TopBar";
 import BrandMark from "./BrandMark";
 import PlayerBar from "./PlayerBar";
 import SongsDrawer from "./SongsDrawer";
+import { tracks } from "../lib/tracks";
 
 export default function RadioExperience() {
   const [trackIndex, setTrackIndex] = useState(0);
   const [songsOpen, setSongsOpen] = useState(false);
 
+  const currentTrack = tracks[trackIndex];
+
   return (
     <>
       <div className="relative">
-        <TopBar onToggleSongs={() => setSongsOpen((o) => !o)} songsOpen={songsOpen} />
+        <TopBar
+          currentTrack={currentTrack}
+          onToggleSongs={() => setSongsOpen((o) => !o)}
+          songsOpen={songsOpen}
+        />
+
         <SongsDrawer
           open={songsOpen}
           activeIndex={trackIndex}
@@ -25,7 +33,10 @@ export default function RadioExperience() {
       <BrandMark />
 
       <div className="player-position">
-        <PlayerBar trackIndex={trackIndex} onTrackIndexChange={setTrackIndex} />
+        <PlayerBar
+          trackIndex={trackIndex}
+          onTrackIndexChange={setTrackIndex}
+        />
       </div>
     </>
   );
